5b16c81c0d360b0381e3e5aa87f197008a8bae2e
SamuelMenard <samuel.menard@videotron.ca>
2018-11-30 13:40:53 -0500

    remplissage grid abo expire

--- Projet2-5B6/Projet2-5B6/Forms/GestionReabonnement/FrmReabonnement.Designer.cs
+++ Projet2-5B6/Projet2-5B6/Forms/GestionReabonnement/FrmReabonnement.Designer.cs
@@ -31,19 +31,19 @@
          this.components = new System.ComponentModel.Container();
          this.lblInformations = new System.Windows.Forms.Label();
          this.lblTitre = new System.Windows.Forms.Label();
-         this.idEtNomCompletAboBindingSource = new System.Windows.Forms.BindingSource(this.components);
          this.idEtNomCompletAboDataGridView = new System.Windows.Forms.DataGridView();
          this.gbAbonne = new System.Windows.Forms.GroupBox();
-         this.label1 = new System.Windows.Forms.Label();
-         this.lblNomAbo = new System.Windows.Forms.Label();
-         this.tbRemarque = new System.Windows.Forms.TextBox();
-         this.btnRenouveler = new System.Windows.Forms.Button();
-         this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
-         this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
          this.label2 = new System.Windows.Forms.Label();
-         ((System.ComponentModel.ISupportInitialize)(this.idEtNomCompletAboBindingSource)).BeginInit();
+         this.btnRenouveler = new System.Windows.Forms.Button();
+         this.tbRemarque = new System.Windows.Forms.TextBox();
+         this.lblNomAbo = new System.Windows.Forms.Label();
+         this.label1 = new System.Windows.Forms.Label();
+         this.idEtNomCompletAboBindingSource = new System.Windows.Forms.BindingSource(this.components);
+         this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+         this.NomComplet = new System.Windows.Forms.DataGridViewTextBoxColumn();
          ((System.ComponentModel.ISupportInitialize)(this.idEtNomCompletAboDataGridView)).BeginInit();
          this.gbAbonne.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.idEtNomCompletAboBindingSource)).BeginInit();
          this.SuspendLayout();
          // 
          // lblInformations
@@ -66,10 +66,6 @@
          this.lblTitre.TabIndex = 8;
          this.lblTitre.Text = "Formulaire de réabonnement";
          // 
-         // idEtNomCompletAboBindingSource
-         // 
-         this.idEtNomCompletAboBindingSource.DataSource = typeof(Projet2_5B6.ObjetsLINQ.IdEtNomCompletAbo);
-         // 
          // idEtNomCompletAboDataGridView
          // 
          this.idEtNomCompletAboDataGridView.AllowUserToAddRows = false;
@@ -77,12 +73,10 @@
          this.idEtNomCompletAboDataGridView.AllowUserToOrderColumns = true;
          this.idEtNomCompletAboDataGridView.AllowUserToResizeColumns = false;
          this.idEtNomCompletAboDataGridView.AllowUserToResizeRows = false;
-         this.idEtNomCompletAboDataGridView.AutoGenerateColumns = false;
          this.idEtNomCompletAboDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
          this.idEtNomCompletAboDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.dataGridViewTextBoxColumn1,
-            this.dataGridViewTextBoxColumn2});
-         this.idEtNomCompletAboDataGridView.DataSource = this.idEtNomCompletAboBindingSource;
+            this.Id,
+            this.NomComplet});
          this.idEtNomCompletAboDataGridView.Location = new System.Drawing.Point(18, 136);
          this.idEtNomCompletAboDataGridView.Name = "idEtNomCompletAboDataGridView";
          this.idEtNomCompletAboDataGridView.ReadOnly = true;
@@ -104,34 +98,15 @@
          this.gbAbonne.TabIndex = 11;
          this.gbAbonne.TabStop = false;
          // 
-         // label1
+         // label2
          // 
-         this.label1.AutoSize = true;
-         this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.label1.Location = new System.Drawing.Point(6, 16);
-         this.label1.Name = "label1";
-         this.label1.Size = new System.Drawing.Size(306, 22);
-         this.label1.TabIndex = 12;
-         this.label1.Text = "Renouvellement de l\'abonnement de ";
-         // 
-         // lblNomAbo
-         // 
-         this.lblNomAbo.AutoSize = true;
-         this.lblNomAbo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.lblNomAbo.Location = new System.Drawing.Point(303, 16);
-         this.lblNomAbo.Name = "lblNomAbo";
-         this.lblNomAbo.Size = new System.Drawing.Size(47, 22);
-         this.lblNomAbo.TabIndex = 13;
-         this.lblNomAbo.Text = "Nom";
-         // 
-         // tbRemarque
-         // 
-         this.tbRemarque.Location = new System.Drawing.Point(10, 66);
-         this.tbRemarque.MaxLength = 100;
-         this.tbRemarque.Multiline = true;
-         this.tbRemarque.Name = "tbRemarque";
-         this.tbRemarque.Size = new System.Drawing.Size(437, 158);
-         this.tbRemarque.TabIndex = 14;
+         this.label2.AutoSize = true;
+         this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label2.Location = new System.Drawing.Point(7, 46);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(97, 17);
+         this.label2.TabIndex = 12;
+         this.label2.Text = "Remarque...";
          // 
          // btnRenouveler
          // 
@@ -143,36 +118,57 @@
          this.btnRenouveler.UseVisualStyleBackColor = true;
          this.btnRenouveler.Click += new System.EventHandler(this.btnRenouveler_Click);
          // 
-         // dataGridViewTextBoxColumn1
+         // tbRemarque
          // 
-         this.dataGridViewTextBoxColumn1.DataPropertyName = "id";
-         this.dataGridViewTextBoxColumn1.HeaderText = "Id";
-         this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-         this.dataGridViewTextBoxColumn1.ReadOnly = true;
+         this.tbRemarque.Location = new System.Drawing.Point(10, 66);
+         this.tbRemarque.MaxLength = 100;
+         this.tbRemarque.Multiline = true;
+         this.tbRemarque.Name = "tbRemarque";
+         this.tbRemarque.Size = new System.Drawing.Size(437, 158);
+         this.tbRemarque.TabIndex = 14;
          // 
-         // dataGridViewTextBoxColumn2
+         // lblNomAbo
          // 
-         this.dataGridViewTextBoxColumn2.DataPropertyName = "nomComplet";
-         this.dataGridViewTextBoxColumn2.HeaderText = "Nom complet";
-         this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
-         this.dataGridViewTextBoxColumn2.ReadOnly = true;
-         this.dataGridViewTextBoxColumn2.Width = 150;
+         this.lblNomAbo.AutoSize = true;
+         this.lblNomAbo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.lblNomAbo.Location = new System.Drawing.Point(303, 16);
+         this.lblNomAbo.Name = "lblNomAbo";
+         this.lblNomAbo.Size = new System.Drawing.Size(47, 22);
+         this.lblNomAbo.TabIndex = 13;
+         this.lblNomAbo.Text = "Nom";
          // 
-         // label2
+         // label1
          // 
-         this.label2.AutoSize = true;
-         this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-         this.label2.Location = new System.Drawing.Point(7, 46);
-         this.label2.Name = "label2";
-         this.label2.Size = new System.Drawing.Size(97, 17);
-         this.label2.TabIndex = 12;
-         this.label2.Text = "Remarque...";
+         this.label1.AutoSize = true;
+         this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label1.Location = new System.Drawing.Point(6, 16);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(306, 22);
+         this.label1.TabIndex = 12;
+         this.label1.Text = "Renouvellement de l\'abonnement de ";
+         // 
+         // idEtNomCompletAboBindingSource
+         // 
+         this.idEtNomCompletAboBindingSource.DataSource = typeof(Projet2_5B6.ObjetsLINQ.IdEtNomCompletAbo);
+         // 
+         // Id
+         // 
+         this.Id.HeaderText = "Id";
+         this.Id.Name = "Id";
+         this.Id.ReadOnly = true;
+         // 
+         // NomComplet
+         // 
+         this.NomComplet.HeaderText = "Nom complet";
+         this.NomComplet.Name = "NomComplet";
+         this.NomComplet.ReadOnly = true;
+         this.NomComplet.Width = 150;
          // 
          // FrmReabonnement
          // 
          this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
          this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-         this.ClientSize = new System.Drawing.Size(835, 484);
+         this.ClientSize = new System.Drawing.Size(826, 445);
          this.Controls.Add(this.gbAbonne);
          this.Controls.Add(this.idEtNomCompletAboDataGridView);
          this.Controls.Add(this.lblInformations);
@@ -180,10 +176,10 @@
          this.Name = "FrmReabonnement";
          this.Text = "FrmReabonnement";
          this.Load += new System.EventHandler(this.FrmReabonnement_Load);
-         ((System.ComponentModel.ISupportInitialize)(this.idEtNomCompletAboBindingSource)).EndInit();
          ((System.ComponentModel.ISupportInitialize)(this.idEtNomCompletAboDataGridView)).EndInit();
          this.gbAbonne.ResumeLayout(false);
          this.gbAbonne.PerformLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.idEtNomCompletAboBindingSource)).EndInit();
          this.ResumeLayout(false);
          this.PerformLayout();
 
@@ -195,13 +191,13 @@
       private System.Windows.Forms.Label lblTitre;
       private System.Windows.Forms.BindingSource idEtNomCompletAboBindingSource;
       private System.Windows.Forms.DataGridView idEtNomCompletAboDataGridView;
-      private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
-      private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
       private System.Windows.Forms.GroupBox gbAbonne;
       private System.Windows.Forms.Button btnRenouveler;
       private System.Windows.Forms.TextBox tbRemarque;
       private System.Windows.Forms.Label lblNomAbo;
       private System.Windows.Forms.Label label1;
       private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+      private System.Windows.Forms.DataGridViewTextBoxColumn NomComplet;
    }
 }
--- Projet2-5B6/Projet2-5B6/Forms/GestionReabonnement/FrmReabonnement.cs
+++ Projet2-5B6/Projet2-5B6/Forms/GestionReabonnement/FrmReabonnement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Projet2_5B6.ObjetsLINQ;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -21,16 +22,73 @@ namespace Projet2_5B6.Forms.GestionReabonnement
 
       private void FrmReabonnement_Load(object sender, EventArgs e)
       {
-         idEtNomCompletAboBindingSource.DataSource = from abonne in data.Abonnements
-                                                     let dateExpiration = abonne.DateAbonnement.AddYears(1)
-                                                     where DateTime.Now > dateExpiration
-                                                     select new
-                                                     {
-                                                        id = abonne.Id,
-                                                        nomComplet = abonne.Prenom + " " + abonne.Nom
-                                                     };
+         Dictionary<string, DateTime> abonnementsDatePlusRecente = new Dictionary<string, DateTime>();
+
+         var abonnementsExpires = from abonne in data.Abonnements
+                                    let dateExpiration = abonne.DateAbonnement.AddYears(1)
+                                    where DateTime.Now > dateExpiration
+                                       select new
+                                       {
+                                          id = abonne.Id,
+                                          dateAbonnement = abonne.DateAbonnement
+                                       };
+
+         var reabonnements = from reabonne in data.Reabonnements
+                             select new
+                              {
+                                 id = reabonne.IdAbbonement,
+                                 dateAbonnement = reabonne.DateRenouvellement
+                              };
+
+         // ajouter les abonnements à la liste
+         foreach (var abo in abonnementsExpires)
+         {
+            abonnementsDatePlusRecente.Add(abo.id, abo.dateAbonnement);
+         }
+
+         // vérifier s'il y a eu des réabonnements
+         foreach (var abo in reabonnements)
+         {
+            DateTime dateAncienneEntree = DateTime.Now;
+            if (abonnementsDatePlusRecente.TryGetValue(abo.id.ToString(), out dateAncienneEntree))
+            {
+               if (dateAncienneEntree < abo.dateAbonnement)
+               {
+                  abonnementsDatePlusRecente[abo.id] = abo.dateAbonnement;
+               }
+            }
+            else
+            {
+               abonnementsDatePlusRecente.Add(abo.id, abo.dateAbonnement);
+            }
+         }
+         
+         // ajouter les données au grid view
+         foreach (KeyValuePair<string, DateTime> entry in abonnementsDatePlusRecente)
+         {
+            System.Diagnostics.Debug.WriteLine("ID : " + entry.Key + " | " + "Date début : " + entry.Value.ToString());
+            var ligne = from abonne in data.Abonnements
+                        where abonne.Id == entry.Key && DateTime.Now > entry.Value.AddYears(1)
+                        select new
+                        {
+                           id = abonne.Id,
+                           nomComplet = abonne.Prenom + " " + abonne.Nom
+                        };
+
+            foreach (var abo in ligne)
+            {
+               string[] row = new string[] { abo.id, abo.nomComplet };
+               idEtNomCompletAboDataGridView.Rows.Add(row);
+            }
+               
+
+         }
+   
+
+
 
          gbAbonne.Visible = false;
+         idEtNomCompletAboDataGridView.ClearSelection();
       }
 
       private void idEtNomCompletAboDataGridView_SelectionChanged(object sender, EventArgs e)
