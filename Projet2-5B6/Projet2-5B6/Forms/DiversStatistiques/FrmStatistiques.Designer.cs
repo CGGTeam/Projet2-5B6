7cbe535e4a50fdd513954ed67863c43c86ec93da
SamuelMenard <samuel.menard@videotron.ca>
2018-12-02 01:27:03 -0500

    moitié des statistiques

--- Projet2-5B6/Projet2-5B6/Forms/DiversStatistiques/FrmStatistiques.Designer.cs
+++ Projet2-5B6/Projet2-5B6/Forms/DiversStatistiques/FrmStatistiques.Designer.cs
@@ -30,20 +30,32 @@
       {
          this.components = new System.ComponentModel.Container();
          Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+         Microsoft.Reporting.WinForms.ReportDataSource reportDataSource2 = new Microsoft.Reporting.WinForms.ReportDataSource();
+         Microsoft.Reporting.WinForms.ReportDataSource reportDataSource3 = new Microsoft.Reporting.WinForms.ReportDataSource();
          this.tabControl1 = new System.Windows.Forms.TabControl();
          this.tabPage1 = new System.Windows.Forms.TabPage();
          this.tabPage2 = new System.Windows.Forms.TabPage();
          this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+         this.reportViewer2 = new Microsoft.Reporting.WinForms.ReportViewer();
          this.ObjetAbonnementsParAnneeEtParTypeAbonnementBindingSource = new System.Windows.Forms.BindingSource(this.components);
+         this.ObjetAbonnementsMoiAnneeCouranteBindingSource = new System.Windows.Forms.BindingSource(this.components);
+         this.tabPage3 = new System.Windows.Forms.TabPage();
+         this.reportViewer3 = new Microsoft.Reporting.WinForms.ReportViewer();
+         this.ObjetPartiesJoueesParAnneeBindingSource = new System.Windows.Forms.BindingSource(this.components);
          this.tabControl1.SuspendLayout();
          this.tabPage1.SuspendLayout();
+         this.tabPage2.SuspendLayout();
          ((System.ComponentModel.ISupportInitialize)(this.ObjetAbonnementsParAnneeEtParTypeAbonnementBindingSource)).BeginInit();
+         ((System.ComponentModel.ISupportInitialize)(this.ObjetAbonnementsMoiAnneeCouranteBindingSource)).BeginInit();
+         this.tabPage3.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.ObjetPartiesJoueesParAnneeBindingSource)).BeginInit();
          this.SuspendLayout();
          // 
          // tabControl1
          // 
          this.tabControl1.Controls.Add(this.tabPage1);
          this.tabControl1.Controls.Add(this.tabPage2);
+         this.tabControl1.Controls.Add(this.tabPage3);
          this.tabControl1.Location = new System.Drawing.Point(0, 30);
          this.tabControl1.Name = "tabControl1";
          this.tabControl1.SelectedIndex = 0;
@@ -63,12 +75,13 @@
          // 
          // tabPage2
          // 
+         this.tabPage2.Controls.Add(this.reportViewer2);
          this.tabPage2.Location = new System.Drawing.Point(4, 22);
          this.tabPage2.Name = "tabPage2";
          this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
          this.tabPage2.Size = new System.Drawing.Size(1505, 597);
          this.tabPage2.TabIndex = 1;
-         this.tabPage2.Text = "tabPage2";
+         this.tabPage2.Text = "Abonnement par mois de l\'année courante et par type d\'abonnement";
          this.tabPage2.UseVisualStyleBackColor = true;
          // 
          // reportViewer1
@@ -77,16 +90,62 @@
          reportDataSource1.Name = "DataSet1";
          reportDataSource1.Value = this.ObjetAbonnementsParAnneeEtParTypeAbonnementBindingSource;
          this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
-         this.reportViewer1.LocalReport.ReportEmbeddedResource = "Projet2_5B6.AssistantsRapportStatistiques.AbonnementsParAnneeEtTypeAbo.rdlc";
+         this.reportViewer1.LocalReport.ReportEmbeddedResource = "Projet2_5B6.AssistantsRapportStatistiques.AbonnementsParAnneeEtTypeAbonnement.rdl" +
+    "c";
          this.reportViewer1.Location = new System.Drawing.Point(3, 3);
          this.reportViewer1.Name = "reportViewer1";
          this.reportViewer1.ServerReport.BearerToken = null;
          this.reportViewer1.Size = new System.Drawing.Size(1499, 591);
          this.reportViewer1.TabIndex = 0;
          // 
+         // reportViewer2
+         // 
+         this.reportViewer2.Dock = System.Windows.Forms.DockStyle.Fill;
+         reportDataSource2.Name = "DataSet1";
+         reportDataSource2.Value = this.ObjetAbonnementsMoiAnneeCouranteBindingSource;
+         this.reportViewer2.LocalReport.DataSources.Add(reportDataSource2);
+         this.reportViewer2.LocalReport.ReportEmbeddedResource = "Projet2_5B6.AssistantsRapportStatistiques.AbonnementsParMoisAnneeCourante.rdlc";
+         this.reportViewer2.Location = new System.Drawing.Point(3, 3);
+         this.reportViewer2.Name = "reportViewer2";
+         this.reportViewer2.ServerReport.BearerToken = null;
+         this.reportViewer2.Size = new System.Drawing.Size(1499, 591);
+         this.reportViewer2.TabIndex = 0;
+         // 
          // ObjetAbonnementsParAnneeEtParTypeAbonnementBindingSource
          // 
          this.ObjetAbonnementsParAnneeEtParTypeAbonnementBindingSource.DataSource = typeof(Projet2_5B6.ObjetsLINQ.ObjetAbonnementsParAnneeEtParTypeAbonnement);
+         // 
+         // ObjetAbonnementsMoiAnneeCouranteBindingSource
+         // 
+         this.ObjetAbonnementsMoiAnneeCouranteBindingSource.DataSource = typeof(Projet2_5B6.ObjetsLINQ.ObjetAbonnementsMoiAnneeCourante);
+         // 
+         // tabPage3
+         // 
+         this.tabPage3.Controls.Add(this.reportViewer3);
+         this.tabPage3.Location = new System.Drawing.Point(4, 22);
+         this.tabPage3.Name = "tabPage3";
+         this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+         this.tabPage3.Size = new System.Drawing.Size(1505, 597);
+         this.tabPage3.TabIndex = 2;
+         this.tabPage3.Text = "Parties jouées par année et par terrain";
+         this.tabPage3.UseVisualStyleBackColor = true;
+         // 
+         // reportViewer3
+         // 
+         this.reportViewer3.Dock = System.Windows.Forms.DockStyle.Fill;
+         reportDataSource3.Name = "DataSet1";
+         reportDataSource3.Value = this.ObjetPartiesJoueesParAnneeBindingSource;
+         this.reportViewer3.LocalReport.DataSources.Add(reportDataSource3);
+         this.reportViewer3.LocalReport.ReportEmbeddedResource = "Projet2_5B6.AssistantsRapportStatistiques.PartiesJoueesParAnneeEtTerrain.rdlc";
+         this.reportViewer3.Location = new System.Drawing.Point(3, 3);
+         this.reportViewer3.Name = "reportViewer3";
+         this.reportViewer3.ServerReport.BearerToken = null;
+         this.reportViewer3.Size = new System.Drawing.Size(1499, 591);
+         this.reportViewer3.TabIndex = 0;
+         // 
+         // ObjetPartiesJoueesParAnneeBindingSource
+         // 
+         this.ObjetPartiesJoueesParAnneeBindingSource.DataSource = typeof(Projet2_5B6.ObjetsLINQ.ObjetPartiesJoueesParAnnee);
          // 
          // FrmStatistiques
          // 
@@ -99,7 +158,11 @@
          this.Load += new System.EventHandler(this.FrmStatistiques_Load);
          this.tabControl1.ResumeLayout(false);
          this.tabPage1.ResumeLayout(false);
+         this.tabPage2.ResumeLayout(false);
          ((System.ComponentModel.ISupportInitialize)(this.ObjetAbonnementsParAnneeEtParTypeAbonnementBindingSource)).EndInit();
+         ((System.ComponentModel.ISupportInitialize)(this.ObjetAbonnementsMoiAnneeCouranteBindingSource)).EndInit();
+         this.tabPage3.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.ObjetPartiesJoueesParAnneeBindingSource)).EndInit();
          this.ResumeLayout(false);
 
       }
@@ -111,5 +174,10 @@
       private System.Windows.Forms.TabPage tabPage2;
       private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
       private System.Windows.Forms.BindingSource ObjetAbonnementsParAnneeEtParTypeAbonnementBindingSource;
+      private Microsoft.Reporting.WinForms.ReportViewer reportViewer2;
+      private System.Windows.Forms.BindingSource ObjetAbonnementsMoiAnneeCouranteBindingSource;
+      private System.Windows.Forms.TabPage tabPage3;
+      private Microsoft.Reporting.WinForms.ReportViewer reportViewer3;
+      private System.Windows.Forms.BindingSource ObjetPartiesJoueesParAnneeBindingSource;
    }
 }
--- Projet2-5B6/Projet2-5B6/Forms/DiversStatistiques/FrmStatistiques.cs
+++ Projet2-5B6/Projet2-5B6/Forms/DiversStatistiques/FrmStatistiques.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,6 +22,8 @@ namespace Projet2_5B6.Forms.DiversStatistiques
 
       private void FrmStatistiques_Load(object sender, EventArgs e)
       {
+
+         // Abonnements par année et par type d’abonnement
          this.ObjetAbonnementsParAnneeEtParTypeAbonnementBindingSource.DataSource = from abonne in data.Abonnements
                                                                                     join typeabo in data.TypeAbonnements on abonne.NoTypeAbonnement equals typeabo.No
                                                                                     select new
@@ -32,7 +35,35 @@ namespace Projet2_5B6.Forms.DiversStatistiques
                                                                                        prenom = abonne.Prenom
                                                                                     };
 
+         // Abonnements par mois de l’année courante et par type d’abonnement
+         this.ObjetAbonnementsMoiAnneeCouranteBindingSource.DataSource = from abonne in data.Abonnements
+                                                                         join typeabo in data.TypeAbonnements on abonne.NoTypeAbonnement equals typeabo.No
+                                                                         where abonne.DateAbonnement.Year == DateTime.Now.Year
+                                                                         select new
+                                                                         {
+                                                                            mois = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(abonne.DateAbonnement.Month),
+                                                                            typeAbonnement = typeabo.Description,
+                                                                            id = abonne.Id,
+                                                                            nom = abonne.Nom,
+                                                                            prenom = abonne.Prenom
+                                                                         };
+
+         // parties jouées par année et par terrain
+         this.ObjetPartiesJoueesParAnneeBindingSource.DataSource =  from partie in data.PartiesJouees
+                                                                     join unTerrain in data.Terrains on partie.NoTerrain equals unTerrain.No
+                                                                     join abonne in data.Abonnements on partie.IdAbbonement equals abonne.Id
+                                                                     select new
+                                                                     {
+                                                                        annee = partie.DatePartie.Year,
+                                                                        nomTerrain = unTerrain.Nom,
+                                                                        datePartie = partie.DatePartie,
+                                                                        nomCompletAbonne = abonne.Prenom + " " + abonne.Nom,
+                                                                        pointage = partie.Pointage
+                                                                     };
+
          this.reportViewer1.RefreshReport();
+         this.reportViewer2.RefreshReport();
+         this.reportViewer3.RefreshReport();
       }
    }
 }
